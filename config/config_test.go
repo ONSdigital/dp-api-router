68502f0ed7e5b0ec330c417717331843e910203c
mikeAdams <mikelovesbooks@gmail.com>
2019-09-02 13:02:02 +0100

    fix config test and fmt

--- config/config_test.go
+++ config/config_test.go
@@ -25,6 +25,6 @@ func TestGetRetrunsDefaultValues(t *testing.T) {
 		So(configuration.APIPocURL, ShouldEqual, "http://localhost:3000")
 		So(configuration.EnvironmentHost, ShouldEqual, "http://localhost:23200")
 		So(configuration.GracefulShutdown, ShouldEqual, time.Second*5)
-		So(configuration.AllowedOrigins, ShouldBeBlank)
+		So(configuration.AllowedOrigins, ShouldEqual, []string{})
 	})
 }
