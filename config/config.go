e12ad3ccc24b0c60b6da8493e8d207974dfc0511
Matt Nicks <matthew.nicks@methods.co.uk>
2021-06-04 11:52:41 +0100

    add IdentityAPIURL to the config

--- config/config.go
+++ config/config.go
@@ -29,6 +29,7 @@ type Config struct {
 	DimensionSearchAPIURL      string        `envconfig:"DIMENSION_SEARCH_API_URL"`
 	ImageAPIURL                string        `envconfig:"IMAGE_API_URL"`
 	UploadServiceAPIURL        string        `envconfig:"UPLOAD_SERVICE_API_URL"`
+	IdentityAPIURL             string        `envconfig:"IDENTITY_API_URL"`
 	ContextURL                 string        `envconfig:"CONTEXT_URL"`
 	EnvironmentHost            string        `envconfig:"ENV_HOST"`
 	APIPocURL                  string        `envconfig:"API_POC_URL"`
@@ -71,6 +72,7 @@ func Get() (*Config, error) {
 			DimensionSearchAPIURL:      "http://localhost:23100",
 			ImageAPIURL:                "http://localhost:24700",
 			UploadServiceAPIURL:        "http://localhost:25100",
+			IdentityAPIURL:             "http://localhost:25600",
 			APIPocURL:                  "http://localhost:3000",
 			ContextURL:                 "",
 			EnvironmentHost:            "http://localhost:23200",
