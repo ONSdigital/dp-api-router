d40a4268f233d3fd0263d642d01a2b0d6fc3915d
Charles Colbourn <charles.colbourn@ext.ons.gov.uk>
2024-01-24 09:40:52 +0000

    added otel instrumentation to proxy transport

--- interceptor/interceptor.go
+++ interceptor/interceptor.go
@@ -14,6 +14,7 @@ import (
 	"strings"
 
 	"github.com/ONSdigital/log.go/v2/log"
+	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
 )
 
 // Transport implements the http RoundTripper method and allows the
@@ -28,7 +29,7 @@ var _ http.RoundTripper = &Transport{}
 
 // NewRoundTripper creates a Transport instance with configured domain
 func NewRoundTripper(domain, contextURL string, rt http.RoundTripper) *Transport {
-	return &Transport{domain, contextURL, rt}
+	return &Transport{domain, contextURL, otelhttp.NewTransport(rt)}
 }
 
 const (
